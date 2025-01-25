66330a47c5f3b26a039efa69a735de959339f044
GaG <francois.prugniel@telecomnancy.net>
2025-01-25 15:51:32 +0100

    sélecteur de couleur et tirette

---
+++ api/ColorDetector.py
@@ -0,0 +1,38 @@
+from gpiozero import Button
+
+class ColorButton(Button):
+    """
+    A class used to represent a Color Button.
+    Inherits from the Button class and adds functionality to detect a specific color state.
+    Attributes
+    ----------
+    pin : int
+        The pin number associated with the button.
+    Methods
+    -------
+    isColor0():
+        Checks if the button is not pressed, indicating the detection of color 0.
+    """
+    
+
+    def __init__(self, pin):
+        """
+        Initialize the ColorDetector with the specified pin.
+        Args:
+            pin (int): The pin number to which the ColorDetector is connected.
+        """
+
+        super().__init__(pin)
+    
+    def is_color_0(self):
+        """
+        Check if the color sensor is not pressed.
+        Returns:
+            bool: True if the color sensor is not pressed, False otherwise.
+        """
+
+        return not self.is_pressed
+    
+# Example usage:
+# color_button = ColorButton(pin=17)
+# print(color_button.is_color_0())
---
+++ api/PullCordDetector.py
@@ -0,0 +1,56 @@
+from gpiozero import Button
+from time import sleep
+
+class PullCordDetector:
+    """
+    A class to detect the state of a pull cord using a button.
+    Attributes:
+    -----------
+    button : Button
+        An instance of the Button class representing the pull cord button.
+    Methods:
+    --------
+    __init__(pin):
+        Initializes the PullCordDetector with the specified pin.
+    get_state():
+        Returns the current state of the button (pressed or not pressed).
+    wait_for_state(expected_state):
+        Waits until the button state matches the expected state.
+    """
+
+
+    def __init__(self, pin):
+        """
+        Initializes the PullCordDetector with the specified pin.
+        Args:
+            pin (int): The GPIO pin number to which the pull cord button is connected.
+        """
+
+        self.button = Button(pin)
+
+    def get_state(self):
+        """
+        Get the current state of the button.
+        Returns:
+            bool: True if the button is pressed, False otherwise.
+        """
+
+        return self.button.is_pressed
+
+    def wait_for_state(self, expected_state):
+        """
+        Waits until the state of the object matches the expected state.
+        This method continuously checks the current state of the object by calling
+        the `get_state` method and compares it to the `expected_state`. It pauses
+        for 0.1 seconds between each check to avoid excessive CPU usage.
+        Args:
+            expected_state: The state that the object is expected to reach.
+        """
+
+        while self.get_state() != expected_state:
+            sleep(0.1)
+
+# Example usage:
+# detector = PullCordDetector(pin=17)
+# print(detector.getState())
+# detector.waitForPullCord(expected_state=True)
