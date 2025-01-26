a4f8f7325b126535b5eb5a0ab1fc1c6f902037d9
GaG <francois.prugniel@telecomnancy.net>
2025-01-26 23:49:45 +0100

    doc

--- ia/tests/TestLidar.py
+++ ia/tests/TestLidar.py
@@ -5,7 +5,40 @@ from api.detection.lidar.Lidar import Lidar
 from asserv.Asserv import Asserv
 
 class TestLidar(AbstractTest):
+    """
+    A test class for the Lidar sensor.
+    This class is used to test the functionality of the Lidar sensor by 
+    initializing it with the configuration data and continuously printing 
+    the detected points.
+    Methods
+    -------
+    test():
+        Initializes the Lidar sensor and continuously prints the detected points.
+    """
+
     def test(self):
+        """
+        Test method for initializing and using the Lidar class.
+        This method initializes a Lidar object with configuration data and continuously
+        prints the detected points from the Lidar sensor every second.
+        Attributes:
+            lidar (Lidar): An instance of the Lidar class initialized with the provided
+                           configuration data.
+        Configuration Data:
+            detection:
+                lidar:
+                    serialPort (str): The serial port for the Lidar.
+                    baudRate (int): The baud rate for the Lidar.
+                    quality (int): The quality setting for the Lidar.
+                    distance (int): The distance setting for the Lidar.
+                    period (int): The period setting for the Lidar.
+            asserv:
+                serialPort (str): The serial port for the Asserv.
+                baudRate (int): The baud rate for the Asserv.
+        Loop:
+            Continuously prints the detected points from the Lidar sensor every second.
+        """
+
         lidar = Lidar(
             serial_port=self.config_data["detection"]["lidar"]["serialPort"], 
             baud_rate=self.config_data["detection"]["lidar"]["baudRate"], 
