195ef08bf7e1d1e9c3a361fd372e3b733b945f4d
GaG <francois.prugniel@telecomnancy.net>
2025-05-11 13:03:57 +0200

    fix enum value

--- ia/asservissement/asserv_status.py
+++ ia/asservissement/asserv_status.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class AsservStatus(Enum):
-    STATUS_IDLE = 1
-    STATUS_RUNNING = 2
-    STATUS_HALTED = 3
-    STATUS_BLOCKED = 4
+    STATUS_IDLE = 0
+    STATUS_RUNNING = 1
+    STATUS_HALTED = 2
+    STATUS_BLOCKED = 3
