13cae85ee4f26ec6d83d1da613164115818a4079
GaG <francois.prugniel@telecomnancy.net>
2025-05-25 21:48:42 +0200

    add server to deploy

--- .build/deploy.sh
+++ .build/deploy.sh
@@ -8,8 +8,8 @@ env | grep DEPLOY
 if [ -z "$DEPLOY_SERVER_IDENTITY_FILE_PATH" ]
 then
     ssh $DEPLOY_SERVER_USER@$DEPLOY_SERVER_HOST "mkdir -p $DEPLOY_SERVER_DIR"
-    rsync -avz --delete -e ssh ia config requirements.txt $DEPLOY_SERVER_USER@$DEPLOY_SERVER_HOST:$DEPLOY_SERVER_DIR
+    rsync -avz --delete -e ssh ia server config requirements.txt $DEPLOY_SERVER_USER@$DEPLOY_SERVER_HOST:$DEPLOY_SERVER_DIR
 else
     ssh -i "$DEPLOY_SERVER_IDENTITY_FILE_PATH" $DEPLOY_SERVER_USER@$DEPLOY_SERVER_HOST "mkdir -p $DEPLOY_SERVER_DIR"
-    rsync -avz --delete -e "ssh -i $DEPLOY_SERVER_IDENTITY_FILE_PATH" ia config requirements.txt $DEPLOY_SERVER_USER@$DEPLOY_SERVER_HOST:$DEPLOY_SERVER_DIR
+    rsync -avz --delete -e "ssh -i $DEPLOY_SERVER_IDENTITY_FILE_PATH" ia server config requirements.txt $DEPLOY_SERVER_USER@$DEPLOY_SERVER_HOST:$DEPLOY_SERVER_DIR
 fi
